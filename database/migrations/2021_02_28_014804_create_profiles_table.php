3bb14b850313996ecd4e7ed5b0ac9c36b2cf10d2
rigikit <rakutena4444@gmail.com>
2021-02-28 03:31:44 +0000

    commit

---
+++ database/migrations/2021_02_28_014804_create_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');//名前
+            $table->string('gender');//性別
+            $table->text('(hobby');//趣味
+            $table->text('introduction');//自己紹介
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
