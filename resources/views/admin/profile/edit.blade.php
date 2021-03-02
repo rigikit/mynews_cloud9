19f26758eac72cf7de3e037cdd617c505ffabd90
rigikit <rakutena4444@gmail.com>
2021-03-02 08:37:48 +0000

    commit

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -45,11 +45,6 @@
                             <div class="form-text text-info">
                                 設定中: {{ $profile_form->image_path }}
                             </div>
-                            <div class="form-check">
-                                <label class="form-check-label">
-                                    <input type="checkbox" class="form-check-input" name="remove" value="true">画像を削除
-                                </label>
-                            </div>
                         </div>
                     </div>
                     <div class="form-group row">
