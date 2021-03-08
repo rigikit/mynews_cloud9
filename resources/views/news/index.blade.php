756a6bd41625c41a49bc31977c6189a47412d038
rigikit <rakutena4444@gmail.com>
2021-03-08 07:26:32 +0000

    commit

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -10,10 +10,10 @@
                         <div class="col-md-6">
                             <div class="caption mx-auto">
                                 <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
-                                </div>
+                                 @if ($headline->image_path)
+                                 <img src="{{ $headline->image_path }}">
+                                 @endif
+                                 </div>
                                 <div class="title p-2">
                                     <h1>{{ str_limit($headline->title, 70) }}</h1>
                                 </div>
@@ -44,10 +44,10 @@
                                 </div>
                             </div>
                             <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif
-                            </div>
+                             @if ($post->image_path)
+                             <img src="{{ $post->image_path }}">
+                             @endif
+                             </div>
                         </div>
                     </div>
                     <hr color="#c0c0c0">
